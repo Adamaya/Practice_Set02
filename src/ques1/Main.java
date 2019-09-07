b91482662ced40fb663d78ce06ff57cc8730315f
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-07 22:58:35 +0530

    rectangle class instance created

--- src/ques1/Main.java
+++ src/ques1/Main.java
@@ -1,5 +1,7 @@
 package ques1;
 
+import ques2.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
         Student student=new Student();
@@ -7,6 +9,8 @@ public class Main {
         student.setRoll_no(101);
         System.out.println(student.getName());
         System.out.println(student.getRoll_no());
+        Rectangle rec=new Rectangle(4.0,5.0);
+        rec.calculateArea();
 
 
     }

b3c862307ffff4bc2a49d01ed7b71311a9017457
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-07 22:55:53 +0530

    main class created and student instance created

---
+++ src/ques1/Main.java
@@ -0,0 +1,10 @@
+package ques1;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student=new Student();
+        student.setName("Aman");
+        student.setRoll_no(101);
+
+    }
+}

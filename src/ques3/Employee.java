837c8322cda2dac70c32981f03488bde8949e0e0
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-07 23:03:51 +0530

    getter setter created

--- src/ques3/Employee.java
+++ src/ques3/Employee.java
@@ -5,5 +5,32 @@ public class Employee {
     private double salary;
     private double numberOfHourWorked;
 
+    public double getNumberOfHourWorked() {
+        return numberOfHourWorked;
+    }
+
+    public void setNumberOfHourWorked(double numberOfHourWorked) {
+        this.numberOfHourWorked = numberOfHourWorked;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Employee(String name){
+        setName(name);
+    }
 
 }

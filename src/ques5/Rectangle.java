979f26950c3125de8b7ff6b29cf0549a9e64f3dc
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-07 23:55:18 +0530

    getter setter and concrete method created

--- src/ques5/Rectangle.java
+++ src/ques5/Rectangle.java
@@ -4,4 +4,31 @@ public class Rectangle {
     private double length;
     private double breadth;
 
+    public double getBreadth() {
+        return breadth;
+    }
+    public double getLength(){
+        return length;
+    }
+    public void setLength(double length){
+        this.length=length;
+    }
+    public void setBreadth(double breadth){
+        this.breadth=breadth;
+    }
+    public Rectangle(){
+        setBreadth(0.0);
+        setLength(0.0);
+    }
+    public Rectangle(double length,double breadth){
+        setBreadth(breadth);
+        setLength(length);
+    }
+    public Rectangle(double breadth){
+        setBreadth(breadth);
+        setLength(0.0);
+    }
+    public double area(){
+        return this.length*this.breadth;
+    }
 }

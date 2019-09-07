845677de43633dedfbec739e5489fa5215d7e341
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-07 22:54:08 +0530

    variable declared and getter setters created

---
+++ src/ques1/Student.java
@@ -0,0 +1,22 @@
+package ques1;
+
+public class Student {
+    private String name;
+    private int roll_no;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRoll_no(int roll_no){
+        this.roll_no=roll_no;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int  getRoll_no(){
+        return roll_no;
+    }
+
+}
+

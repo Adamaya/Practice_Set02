3b2846f2bd5a16cc92b4a05eef65a89e525a95d9
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-08 01:07:41 +0530

    getter created

--- src/ques7/PiggyBank.java
+++ src/ques7/PiggyBank.java
@@ -2,4 +2,14 @@ package ques7;
 
 public class PiggyBank {
     private double amount;
+    public double getAmount(){
+        return amount;
+    }
+    protected final void takeMoney(double money){
+        amount+=money;
+    }
+    protected final void addMoney(double money){
+        amount-=money;
+    }
+
 }

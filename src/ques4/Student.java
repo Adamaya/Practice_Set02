68fe2efca629365c07675582758e772a4603ee54
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-07 23:27:35 +0530

    getter setter created

--- src/ques4/Student.java
+++ src/ques4/Student.java
@@ -4,4 +4,19 @@ public class Student {
     private String name;
     private double marks;
 
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
 }

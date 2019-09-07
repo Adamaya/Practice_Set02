cced22756321858de8595aac5831e3cd4bbbd39e
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-08 00:03:40 +0530

    print statement added

--- src/ques5/Main.java
+++ src/ques5/Main.java
@@ -7,6 +7,8 @@ public class Main {
         Rectangle rect1=new Rectangle();
         Rectangle rect2=new Rectangle(4.0,4.0);
         Rectangle rect3=new Rectangle(4.0);
-
+        System.out.println(rect1.area());
+        System.out.println(rect2.area());
+        System.out.println(rect3.area());
     }
 }

2e4c385e5f400e21543f153cd6ffca01da23847d
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-07 23:00:40 +0530

    getter setter and constructor created

--- src/ques2/Rectangle.java
+++ src/ques2/Rectangle.java
@@ -4,6 +4,27 @@ public class Rectangle {
     private double length;
     private double breadth;
 
+    public Rectangle(double length, double breadth) {
+        setBreadth(breadth);
+        setLength(length);
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getBreadth() {
+        return breadth;
+    }
+
+    public void setBreadth(double breadth) {
+        this.breadth = breadth;
+    }
+
 
 
 }

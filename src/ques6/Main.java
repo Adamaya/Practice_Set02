548df2c48e4974bd40518b07ea9e9d472b6717ef
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-08 00:17:52 +0530

    psvm created and class imported

---
+++ src/ques6/Main.java
@@ -0,0 +1,7 @@
+package ques6;
+import ques6.Programming
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ques6/Programming.java
@@ -0,0 +1,10 @@
+package ques6;
+
+public class Programming {
+    public Programming(){
+        System.out.println("I love programming");
+    }
+    public Programming(String pname){
+        System.out.println("I love programming in "+pname);
+    }
+}

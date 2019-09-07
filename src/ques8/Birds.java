ecb8fbfffa75248a5d901853ec614edba05fe8db
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-08 01:39:16 +0530

    fly method created and eat,sleep method overrided

--- src/ques8/Birds.java
+++ src/ques8/Birds.java
@@ -1,5 +1,18 @@
 package ques8;
 
 public class Birds extends Animal {
+    public void fly() {
+        System.out.println("Birds flies in the sky");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Birds eat insects");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("birds do not sleep");
+    }
 
 }

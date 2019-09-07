c75f9e06d54242de5e8a3888199de72e8874ec8e
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-08 00:20:51 +0530

    Scanner object created

--- src/ques6/Main.java
+++ src/ques6/Main.java
@@ -1,7 +1,12 @@
 package ques6;
-import ques6.Programming
+import ques6.Programming;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String pname=input.nextLine();
 
     }
 }

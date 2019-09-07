5a8dcad3abd5106eb34db79d19685d57a62a0dcf
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-08 01:36:49 +0530

    Animal class created and eat and sleep method created

---
+++ src/ques8/Animal.java
@@ -0,0 +1,11 @@
+package ques8;
+
+public class Animal {
+    public void eat() {
+        System.out.println("Animals eat grass");
+    }
+
+    public void sleep() {
+        System.out.println("zZzZzZzZ");
+    }
+}

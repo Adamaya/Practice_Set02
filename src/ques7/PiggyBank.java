b3610c95105819326bd8c8c234684f5cd19a6424
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-08 01:20:10 +0530

    print statement added in parametrised constructor

--- src/ques7/PiggyBank.java
+++ src/ques7/PiggyBank.java
@@ -8,7 +8,7 @@ public class PiggyBank {
 
     PiggyBank(double money) {
         addMoney(money);
-        getAmount();
+        System.out.println(getAmount());
     }
 
     protected final void takeMoney(double money){

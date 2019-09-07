6acb2e4b41f5723bd15b30688d53208fad1391aa
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-08 00:03:21 +0530

    Rectangle instances created

--- src/ques5/Main.java
+++ src/ques5/Main.java
@@ -1,9 +1,12 @@
 package ques5;
 
-import ques5.Main;
+import ques5.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
+        Rectangle rect1=new Rectangle();
+        Rectangle rect2=new Rectangle(4.0,4.0);
+        Rectangle rect3=new Rectangle(4.0);
 
     }
 }
